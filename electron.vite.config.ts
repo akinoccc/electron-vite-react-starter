cb52aadc200072322b9b7d49684f98fb2dfe2784
AkinoChen <akinochen@foxmail.com>
2023-03-07 16:17:13 +0800

    chore: add alias resolver

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   main: {
@@ -27,7 +28,7 @@ export default defineConfig({
     root: '.',
     resolve: {
       alias: {
-        '@src': resolve('src'),
+        '@renderer': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
     build: {
