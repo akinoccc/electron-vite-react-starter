222f298c02bc1ab8f808f806871d0452181d3f68
AkinoChen <akinochen@foxmail.com>
2023-03-07 10:04:55 +0800

    chore: change the eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    'prettier/prettier': 'error',
   },
   overrides: [
     {
