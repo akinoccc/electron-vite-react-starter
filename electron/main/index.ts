617d90f9ed6645163aece4e68626057bb9c82e68
AkinoChen <akinochen@foxmail.com>
2023-03-08 15:51:35 +0800

    perf: remove the default action of opening devtool

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -32,8 +32,6 @@ function createWindow(): void {
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
   }
-
-  mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
