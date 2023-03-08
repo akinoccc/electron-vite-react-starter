c3da0caa8c9b8d4ff2881298b6b0ed89c80bc346
AkinoChen <akinochen@foxmail.com>
2023-03-08 10:32:38 +0800

    feat: add reset css

--- src/APP.tsx
+++ src/APP.tsx
@@ -1,8 +1,14 @@
-import routes from '@renderer/routers';
+import routes from './routers';
 import { RouteObject, useRoutes } from 'react-router-dom';
+import Reset from './assets/styles/reset';
 
 function App(): JSX.Element {
-  return <>{useRoutes(routes as RouteObject[])}</>;
+  return (
+    <>
+      {useRoutes(routes as RouteObject[])}
+      <Reset />
+    </>
+  );
 }
 
 export default App;
---
+++ src/assets/styles/reset.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+export default createGlobalStyle`
+html, body, ul, li, ol, dl, dd, dt, p, h1, h2, h3, h4, h5, h6, form, fieldset, legend, img { margin:0; padding:0; }
+
+fieldset, c{ border:none; }
+
+img{display: block;}
+
+address, caption, cite, code, dfn, th, var { font-style:normal; font-weight:normal; }
+
+ul, ol ,li{ list-style:none; }
+
+body { color:#333; font:12px BASE "SimSun","宋体","Arial Narrow",HELVETICA; background:#fff;}
+
+a { color:#666; text-decoration:none; }
+
+*{box-sizing:border-box}
+
+body,html,#root{
+    height: 100%;
+    overflow: hidden;
+}`;
