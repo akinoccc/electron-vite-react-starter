7f8e7ca61187d4e3d2f0bce344b409fe35728183
AkinoChen <akinochen@foxmail.com>
2023-03-08 15:48:38 +0800

    perf: remove the style "overflow: hidden" on body

--- src/assets/styles/reset.ts
+++ src/assets/styles/reset.ts
@@ -29,5 +29,33 @@ a { color:#666; text-decoration:none; }
 
 body,html,#root{
     height: 100%;
-    overflow: hidden;
-}`;
+}
+
+::-webkit-scrollbar{width:6px;height:4px;border-radius: 3px}
+::-webkit-scrollbar-button{width:6px;height:0;border-radius: 3px}
+::-webkit-scrollbar-track{background:0 0}
+::-webkit-scrollbar-thumb{background:#245FD9;-webkit-transition:.3s;transition:.3s}
+::-webkit-scrollbar-thumb:hover{background-color:#245FD9}
+::-webkit-scrollbar-thumb:active{background-color:#245FD9}*/
+  ::-webkit-scrollbar {
+      width: 6px;
+      height: 6px;
+  }
+::-webkit-scrollbar-track-piece{
+    background-color: transparent;
+        -webkit-border-radius: 3px;
+}
+::-webkit-scrollbar-thumb:vertical{
+    height: 6px;
+    background-color: #D0D0D0;
+    -webkit-border-radius: 3px;
+}
+::-webkit-scrollbar-thumb:horizontal{
+    width: 6px;
+    background-color: #D0D0D0;
+    -webkit-border-radius: 3px;
+}
+::-webkit-scrollbar-thumb:vertical:hover,::-webkit-scrollbar-thumb:horizontal:hover{
+    background-color: #D0D0D0;
+}
+`;
