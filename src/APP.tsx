1ab8805da85550cd296a42fb134f10f5e2922ea6
AkinoChen <akinochen@foxmail.com>
2023-03-08 14:38:35 +0800

    fix: router error

--- src/APP.tsx
+++ src/APP.tsx
@@ -1,13 +1,15 @@
 import routes from './routers';
 import { RouteObject, useRoutes } from 'react-router-dom';
 import Reset from './assets/styles/reset';
+import { Suspense } from 'react';
+import { Spin } from 'antd';
 
 function App(): JSX.Element {
   return (
-    <>
+    <Suspense fallback={<Spin tip="Loading..." />}>
       {useRoutes(routes as RouteObject[])}
       <Reset />
-    </>
+    </Suspense>
   );
 }
 
